53a0f2691a5de09908be3d440dc8be998bc73390
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-14 13:57:44 -0300

    crud completo

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -12,8 +12,10 @@ class UserController{
         let{name,email,password,birth} = req.body
         let result = await User.createUser(name,email,password,birth)
         .catch(error=>{
+
             console.log("falha na entrada do banco")
             return res.status(400).json({error:"Falha ao cadastrar Usuario"})
+
         })
         if (result.status == 0){
 
@@ -28,22 +30,27 @@ class UserController{
         let {email,password} = req.body
         let user = await User.findByEmail(email)
         .catch(error=>{
-            console.log("fallha ao obter email")
+
+            console.log("falha ao obter email")
             return res.status(400).json({message:"Email não encontrado"})
         })
 
         if (user != undefined){
+
             let result = await bcrypt.compare(password,user.password)
 
             if(result){
+
                 let token = jwt.sign({email:user.email},secret)
                 return res.json({token:token})
 
             }else{
+
                 return res.status(406).json({error:"Senha incorreta"})
             }
 
         }else{
+
             return res.status(400).json({error:"Usuario não encontrado"})
         }
     }
@@ -52,6 +59,7 @@ class UserController{
 
         let users = await User.findAll()
         .catch(error=>{
+
             console.log(error)
             return res.status(400).json({error:"Falha na listagem de usuários"})
         })
@@ -64,16 +72,31 @@ class UserController{
         let {id} = req.body
         let result = await User.update(id)
         .catch(error=>{
+
             console.log(error)
             return res.status(400).json({error:"Falha na alteração do email"})
         })
+
         if (result != undefined){
 
             return res.json({response:"Email altualizado com sucesso!"})
+
         }else{
             
             return res.status(400).json({error:"Falha na alteração do email"})
         }
+    }
+
+    async deleteUser(req,res){
+
+        let {id} = req.body
+        let result = await User.delete(id)
+        .catch(error=>{
+            console.log(error)
+            return res.status(400).json({error:error})
+        })
+
+        return res.json({message:"Usuario deletado com sucesso!",response:result})
     }
 }
 module.exports = new UserController();
--- src/models/User.js
+++ src/models/User.js
@@ -3,63 +3,89 @@ const bcrypt = require("bcrypt");
 
 class User {
 
-    async createUser(name,email,password,birth){
-        console.log("#################################")
-        let hash = await bcrypt.hash(password,10)
-        .catch(error=>{
-            console.log("Falha em gerar hash")
-        });
-        console.log("-------->",hash)
+    async createUser(name, email, password, birth) {
+        
+        let hash = await bcrypt.hash(password, 10)
+            .catch(error => {
 
-        let result = await knex.insert({name:name,email:email,password:hash,birth:birth})
-        .table("users")
-        .catch(error=>{
-            console.log("falha ao criar novo usuario")
-            return {status:0}
-        })
-        console.log(`SAIDA DO BANCO: ${result}`,result)
-        return {status:1}
+                console.log("Falha em gerar hash")
+            });
+
+        console.log("-------->", hash)
+
+        let result = await knex.insert({ name: name, email: email, password: hash, birth: birth })
+            .table("users")
+            .catch(error => {
+
+                console.log("falha ao criar novo usuario")
+                return { status: 0 }
+            })
+        console.log(`SAIDA DO BANCO: ${result}`, result)
+        return { status: 1 }
 
     }
 
-    async findByEmail(email){
-        let result = await knex.select(["id","email","password","name"])
-        .from("users")
-        .where({email:email})
-        .catch(error=>{
-            console.log("Falha em obter dados a partir do email")
-            return undefined
-        })
-        
-        if(result.length > 0 ){
+    async findByEmail(email) {
+
+        let result = await knex.select(["id", "email", "password", "name"])
+            .from("users")
+            .where({ email: email })
+            .catch(error => {
+
+                console.log("Falha em obter dados a partir do email")
+                return undefined
+            })
+
+        if (result.length > 0) {
+
             console.log(result)
             return result[0]
-        }else{
+
+        } else {
+
             return undefined
         }
     }
 
-    async findAll(){
-        
-        let result = await knex.select(["id","name","email"])
-        .from("users")
-        .catch(error=>{
-            console.log(erro)
-        })
+    async findAll() {
+
+        let result = await knex.select(["id", "name", "email"])
+            .from("users")
+            .catch(error => {
+
+                console.log(erro)
+            })
 
         return result
-    
+
     }
 
-    async update(id){
-        let result = await knex.update({email:email})
+    async update(id) {
+
+        let result = await knex.update({ email: email })
+            .where({ id: id })
+            .table("users")
+            .catch(error => {
+
+                console.log(error)
+                return undefined
+            })
+
+        return result
+    }
+
+    async delete(id){
+
+        let result = await knex.delete()
+        .from("users")
         .where({id:id})
-        .table("users")
         .catch(error=>{
+
             console.log(error)
             return undefined
         })
-        return result
+
+        return {status:1,response:result}
     }
 }
 module.exports = new User();
