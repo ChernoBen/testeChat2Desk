151cd36c338b551605a0aad7ea15c72e3c244cc0
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-15 19:56:38 -0300

    documentação api

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const app = express();
 const router = require("./routes/routes");
 const cors = require("cors");
-const expressSwagger = require('express-swagger-generator')(app);
-
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,35 +13,8 @@ app.use(bodyParser.json())
 
 
 app.use("/",router);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-
-let options = {
-    swaggerDefinition: {
-        info: {
-            description: 'This is a sample server',
-            title: 'Swagger',
-            version: '1.0.0',
-        },
-        host: 'localhost:8686',
-        basePath: '/v1',
-        produces: [
-            "application/json",
-            "application/xml"
-        ],
-        schemes: ['http', 'https'],
-        securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
-    },
-    basedir: __dirname, //app absolute path
-    files: ['./routes/*.js'] //Path to the API handle folder
-};
-expressSwagger(options)
 app.listen(8686,()=>{
 	console.log("Server running ...")
 });
