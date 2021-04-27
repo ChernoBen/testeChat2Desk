415fc46be7f30744c0337051d5e886eade01891a
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-27 11:00:51 -0300

    docker file 100% funcional

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # Especificar imagem base primeiro nome do repositorio:tag(versao)
 FROM node:alpine
 
+# Definindo diretorio de trabalho dentro do container
+WORKDIR /usr/app
+
 #Copiando arquivos do diretorio atual para o diretorio do container
 COPY ./ ./
 # instalar as dependencias
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger_output.json');
 require('dotenv/config');
-const port = parseInt(process.env.API_PORT);
+//const port = parseInt(process.env.API_PORT);
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use("/",router);
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.listen(port,()=>{
+app.listen(parseInt(process.env.API_PORT),()=>{
 	console.log("Server running ...")
 });
 
