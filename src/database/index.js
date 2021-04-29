c7f69d92c9b3e30b520ac3afbf3c4bf2db388387
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-29 14:42:29 -0300

    knex create schemma

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 const knexfile = require("../../knexfile");
 const knex = require("knex")(knexfile.development)
+require('dotenv/config')
 
+knex.schema.raw(process.env.DATABASE)
 module.exports = knex
